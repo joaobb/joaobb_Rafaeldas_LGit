33539fd89a6c7cb2d3e8e20d076aaf5780d35a37
joaop <joaopedro.bb98@gmail.com>
2018-06-07 13:41:46 -0300

    Inicio de recepcao teste

---
+++ src/hotel/Recepcao.java
@@ -0,0 +1,15 @@
+package hotel;
+
+import java.util.ArrayList;
+
+public class Recepcao {
+    //ArrayList<Estadia> estadias;
+
+    public Recepcao(){
+        //this.estadias = new ArrayList<>();
+    }
+
+    public void checkin(String nome, String tipo, int idade, int dias, double valor){
+
+    }
+}
